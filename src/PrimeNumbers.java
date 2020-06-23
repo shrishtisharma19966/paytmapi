8920b26543b4d3390937f1daf2a41b83afcb4cc0
shrishtisharma19966 <67260566+shrishtisharma19966@users.noreply.github.com>
2020-06-23 14:41:41 +0530

    Prime No.

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,30 @@
+package com.shrishti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumbers {
+	public static void main(String[] args) {
+
+		List<Integer> primeNo = new ArrayList<Integer>();
+		primeNo.add(2);
+
+		int num = 3;
+		while (primeNo.size() < 100) {
+			boolean flag = true;
+			for (int i = 0; i < primeNo.size(); i++) {
+				if ((num % primeNo.get(i)) == 0) {
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag) {
+				primeNo.add(num);
+			}
+			num++;
+		}
+		System.out.println(primeNo);
+
+	}
+}
